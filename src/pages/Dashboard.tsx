4ba3d203b6f0199faddb4d3d86317950a1e4c9df
atzulf <atzulfikar45@gmail.com>
2026-01-02 16:16:01 +0700

    feat(Dashboard): add Category component to Dashboard for enhanced product display

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,6 @@
-import { div } from "motion/react-client";
 import { useEffect, useState } from "react";
 import BannerSlider from "../components/BannerSlider";
+import Category from "../components/Category";
 
 const Dashboard = () => {
     const [user, setUser] = useState<{ name: string; email: string } | null>(null);
@@ -17,6 +17,7 @@ const Dashboard = () => {
         <div className="mt-15 mx-auto max-w-7xl">
             <div className="rounded-lg">
                 <BannerSlider />
+                <Category />
             </div>
         </div>
     );
