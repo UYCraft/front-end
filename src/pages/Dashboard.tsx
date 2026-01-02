e758ca5687939b9f2bfa8d0fab44899f97d6482f
atzulf <atzulfikar45@gmail.com>
2026-01-02 13:20:59 +0700

    feat(Dashboard): implement user state management and integrate BannerSlider component

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,8 +1,25 @@
+import { div } from "motion/react-client";
+import { useEffect, useState } from "react";
+import BannerSlider from "../components/BannerSlider";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+    const [user, setUser] = useState<{ name: string; email: string } | null>(null);
 
-export default Dashboard
+    useEffect(() => {
+        // Ambil data user yang tadi disimpan saat Login
+        const storedUser = localStorage.getItem("user");
+        if (storedUser) {
+            setUser(JSON.parse(storedUser));
+        }
+    }, []);
+
+    return (
+        <div className="mt-15 mx-auto max-w-7xl">
+            <div className="rounded-lg">
+                <BannerSlider />
+            </div>
+        </div>
+    );
+};
+
+export default Dashboard;
